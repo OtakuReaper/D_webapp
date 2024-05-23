fe76a51138215680dae1dda8dc3bb16ecc486910
Michael <mikegomez122@gmail.com>
2024-05-22 19:49:56 -0600

    did the homework

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
-app.use(express)
+app.set("view engine", "ejs");
 
-app.set("public", path.join(__dirname, "public"));
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.render("index", {title: "Subscribe", message: "Hello World"});
+});
+
+app.get("/hello",(req, res)=> {
+    res.send("hello world");
 });
 
 app.listen(port, () => {
